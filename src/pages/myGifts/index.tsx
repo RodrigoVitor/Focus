91813a1725a544c34efa3b27af0b554655a14270
RodrigoVitor <rodrigovitor400@gmail.com>
2024-08-20 07:47:50 -0300

    Resgatando minhas recompensas

--- src/pages/myGifts/index.tsx
+++ src/pages/myGifts/index.tsx
@@ -1,43 +1,34 @@
+import { useEffect, useState } from "react"
+
+type GiftType = string[]
+
 export function MyGifts () {
+    const [gifts, setGifts] = useState<string[]>([])
+
+    useEffect(() => {
+        getMyGifts()
+    }, [])
+    function getMyGifts() {
+        const myGifts = JSON.parse(localStorage.getItem('MyGifts')!)
+        setGifts(myGifts)
+    }
+
     return (
         <div className="min-h-screen pb-10 sm:pb-2 pt-10 flex flex-col justify-center items-center">
-           <div className="grid sm:grid-cols-3 lg:grid-cols-4 gap-5">
-            <div className="text-center">
-                <div className="w-48 text-center bg-blue-900 mt-10 mb-4 py-4">
-                    <div className="flex flex-col mt-3">
-                        <span className="text-zinc-50 text-2xl">1 dia de folga</span>
-                    </div>
+            {gifts && 
+                <div className="grid sm:grid-cols-3 lg:grid-cols-4 gap-5">
+                    {gifts.map((g: string, index: number) => (
+                        <div key={index} className="text-center">
+                                <div className="w-48 text-center bg-blue-900 mt-10 mb-4 py-4">
+                                    <div className="flex flex-col mt-3">
+                                        <span className="text-zinc-50 text-2xl">{g}</span>
+                                    </div>
+                                </div>
+                                <button className="text-zinc-50 bg-blue-900 p-2 rounded-xl sm:text-xl hover:bg-slate-700 w-full">Concluido</button>
+                            </div>
+                        ))}
                 </div>
-                <button className="text-zinc-50 bg-blue-900 p-2 rounded-xl sm:text-xl hover:bg-slate-700 w-full">Concluido</button>
-            </div>
-
-            <div className="text-center">
-                <div className="w-48 text-center bg-blue-900 mt-10 mb-4 py-4">
-                    <div className="flex flex-col mt-3">
-                        <span className="text-zinc-50 text-2xl">1 dia de folga</span>
-                    </div>
-                </div>
-                <button className="text-zinc-50 bg-blue-900 p-2 rounded-xl sm:text-xl hover:bg-slate-700 w-full">Concluido</button>
-            </div>
-
-            <div className="text-center">
-                <div className="w-48 text-center bg-blue-900 mt-10 mb-4 py-4">
-                    <div className="flex flex-col mt-3">
-                        <span className="text-zinc-50 text-2xl">1 dia de folga</span>
-                    </div>
-                </div>
-                <button className="text-zinc-50 bg-blue-900 p-2 rounded-xl sm:text-xl hover:bg-slate-700 w-full">Concluido</button>
-            </div>
-
-            <div className="text-center">
-                <div className="w-48 text-center bg-blue-900 mt-10 mb-4 py-4">
-                    <div className="flex flex-col mt-3">
-                        <span className="text-zinc-50 text-2xl">1 dia de folga</span>
-                    </div>
-                </div>
-                <button className="text-zinc-50 bg-blue-900 p-2 rounded-xl sm:text-xl hover:bg-slate-700 w-full">Concluido</button>
-            </div>
-           </div>
+            }
         </div>
     )
 }
